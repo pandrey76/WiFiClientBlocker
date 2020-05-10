0f0fc027d096d06489d5028faa6f923e4eb03c77
pandrey76 <pandrey76@yandex.ru>
2020-05-11 00:59:30 +0300

    Now browser run by only keyboard keys pressing

--- run-browser.py
+++ run-browser.py
@@ -1,6 +1,7 @@
 
 import pyautogui as pg
 import time
+from auth import password
 
 pg.moveTo(200, 200, 0.5)
 pg.drag(50, 50, 0.5, button='left')
@@ -15,9 +16,20 @@ pg.click(350, 350)
 
 # 
 pg.hotkey("winleft")
-pg.moveTo(20, 180, 0.5)
-pg.moveTo(250, 180, 0.5)
-pg.leftClick()
+# Run browser
+# ################
+# pg.moveTo(20, 180, 0.5)
+# pg.moveTo(250, 180, 0.5)
+# pg.leftClick()
+
+i = 0
+while i<5:
+    pg.press("down")
+    i+=1
+pg.press("right")
+pg.press("enter")
+# ################
+
 time.sleep(3)
 # pg.typewrite("www.youtube.com", 0.3)
 # pg.typewrite(["enter"])
@@ -29,7 +41,7 @@ pg.typewrite("admin", 0.1)
 #pg.moveTo(x, y + 30)
 #pg.leftClick()1
 pg.press("tab")
-pg.typewrite("password", 0.1)
+pg.typewrite(password, 0.1)
 pg.press("enter")
 time.sleep(2)
 i = 0
