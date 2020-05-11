e9ff5c0cc3a67a5ce83cf9903a636b3f211bb1da
pandrey76 <pandrey76@yandex.ru>
2020-05-11 14:30:59 +0300

    Make some changing for working entering to router admin through web interface.

--- run-browser.py
+++ run-browser.py
@@ -3,10 +3,10 @@ import pyautogui as pg
 import time
 from auth import password
 
-pg.moveTo(200, 200, 0.5)
-pg.drag(50, 50, 0.5, button='left')
-print(pg.position())
-pg.click(350, 350)
+# pg.moveTo(200, 200, 0.5)
+# pg.drag(50, 50, 0.5, button='left')
+# print(pg.position())
+# pg.click(350, 350)
 
 #pg.typewrite("Hello, world!")
 #pg.typewrite(["enter"])
@@ -30,12 +30,12 @@ pg.press("right")
 pg.press("enter")
 # ################
 
-time.sleep(3)
+time.sleep(5)
 # pg.typewrite("www.youtube.com", 0.3)
 # pg.typewrite(["enter"])
-pg.typewrite("http://192.168.1.254", 0.1) # 0.3)
+pg.typewrite("http://192.168.1.254/index.htm", 0.1) # 0.3)
 pg.press("enter")
-time.sleep(2)
+time.sleep(10)
 pg.typewrite("admin", 0.1)
 # x, y = pg.position()
 #pg.moveTo(x, y + 30)
@@ -43,16 +43,16 @@ pg.typewrite("admin", 0.1)
 pg.press("tab")
 pg.typewrite(password, 0.1)
 pg.press("enter")
-time.sleep(2)
+time.sleep(20)
 i = 0
 while i < 15:
     pg.press("tab")
     i += 1
 
 pg.press("enter")
-time.sleep(2)
+time.sleep(1)
 pg.press("enter")
-time.sleep(2)
+time.sleep(5)
 pg.hotkey("alt", "f4")
 
 
