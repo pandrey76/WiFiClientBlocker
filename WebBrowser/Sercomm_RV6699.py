1c70606d7bcded8d2ae3a77739c3a4f237567638
pandrey76 <pandrey76@yandex.ru>
2020-05-21 15:10:42 +0300

    Correcting modules import code.

--- WebBrowser/Sercomm_RV6699.py
+++ WebBrowser/Sercomm_RV6699.py
@@ -2,6 +2,7 @@ import os
 import pyautogui as pg
 from time import sleep
 import importlib.util
+import sys
 # from IRouterWebInterfaceController import IRouterWebInterfaceController
 
 base_module_name = "IRouterWebInterfaceController"
@@ -11,6 +12,7 @@ path_to_scripts = os.path.dirname(os.path.realpath(__file__))
 path_to_scripts = os.path.join(path_to_scripts, base_module_name + '.py')
 spec = importlib.util.spec_from_file_location(base_module_name, path_to_scripts)
 base_module = importlib.util.module_from_spec(spec)
+sys.modules[base_module_name] = base_module
 spec.loader.exec_module(base_module)
 
 base_class = getattr(base_module, base_class_name)
