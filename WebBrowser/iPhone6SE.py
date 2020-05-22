0567dfc3004054a1651eedb81045917ff17b8196
pandrey76 <pandrey76@yandex.ru>
2020-05-22 14:48:08 +0300

    Added another router.
    Created object that implement routers API for iPhone router.

---
+++ WebBrowser/iPhone6SE.py
@@ -0,0 +1,67 @@
+import os
+import pyautogui as pg
+from time import sleep
+import importlib.util
+import sys
+# from IRouterWebInterfaceController import IRouterWebInterfaceController
+
+base_module_name = "IRouterWebInterfaceController"
+base_class_name = "IRouterWebInterfaceController"
+
+path_to_scripts = os.path.dirname(os.path.realpath(__file__))
+path_to_scripts = os.path.join(path_to_scripts, base_module_name + '.py')
+spec = importlib.util.spec_from_file_location(base_module_name, path_to_scripts)
+base_module = importlib.util.module_from_spec(spec)
+sys.modules[base_module_name] = base_module
+spec.loader.exec_module(base_module)
+
+base_class = getattr(base_module, base_class_name)
+
+
+class iPhone6SE(base_class):
+
+    def __init__(self, url, login, password):
+        """
+
+        :param url:
+        :param login:
+        :param password:
+        """
+        super().__init__(url, login, password)
+
+    def blocking(self):
+        """
+
+        :return:
+        """
+        # Run browser
+        self.run_default_browser()
+
+        # Login admin
+        sleep(2)
+        self.login_administrator()
+
+        # Make blocking actions
+        sleep(2)
+        i = 0
+        while i < 15:
+            pg.press("tab")
+            i += 1
+
+        pg.press("enter")
+        sleep(2)
+        pg.press("enter")
+        sleep(2)
+        base_class.close_active_window()
+
+    def unblocking(self):
+        """
+
+        :return:admin
+        """
+        return None
+
+
+if __name__ == '__main__':
+    obj = iPhone6SE("http://IP/index.htm", "admin", "")
+    obj.blocking()
