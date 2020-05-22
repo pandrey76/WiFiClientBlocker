e7d45ea0e52f154a08e344c28456721dd92ee200
pandrey76 <pandrey76@yandex.ru>
2020-05-22 14:43:22 +0300

    Correcting code for scheduler.

--- SchedulerScriptRun.py
+++ SchedulerScriptRun.py
@@ -1,16 +1,19 @@
 import schedule
 import os
 from time import sleep
+from RoutersInspector import RoutersInspector
 
 
 def job():
-    os.system(
-        "/home/admin1/work/WiFiClientBlocker/venv/bin/python3 /home/admin1/work/WiFiClientBlocker/RoutersInspector.py")
+    # os.system( "/home/admin1/work/WiFiClientBlocker/venv/bin/python3
+    # /home/admin1/work/WiFiClientBlocker/RoutersInspector.py")
+
+    obj = RoutersInspector()
+    obj.turn_off()
 
 
 schedule.every(1).minutes.do(job)
 
 while True:
     schedule.run_pending()
-    sleep(1)
-
+    # sleep(1)
