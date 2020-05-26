ed8d93f7d0c44f0b4a4b3f4aa9ac9ddd7d8221bc
pandrey76 <pandrey76@yandex.ru>
2020-05-26 15:35:34 +0300

    Added IServer.py file.

---
+++ IServer.py
@@ -0,0 +1,25 @@
+
+
+class IServer:
+    """
+
+    """
+    def __init__(self):
+        """
+
+        """
+        pass
+
+    def request(self):
+        """
+
+        :return:
+        """
+        pass
+
+    def get_response(self):
+        """
+
+        :return:
+        """
+        pass
