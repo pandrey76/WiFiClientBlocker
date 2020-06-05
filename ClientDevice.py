0dc2a3201b413e44c31f18f31d39d8ccb3b5a42a
pandrey76 <pandrey76@yandex.ru>
2020-06-05 10:16:04 +0300

    Added ClientDevice.py file.

---
+++ ClientDevice.py
@@ -0,0 +1,45 @@
+
+
+class ClientDevice:
+    """
+
+    """
+
+    def __int__(self, name, mac, long_name, priority, ip):
+        """
+
+        :param name:
+        :param mac:
+        :param long_name:
+        :param priority:
+        :param ip:
+        :return:
+        """
+        self.__Name = name
+        self.__Mac = mac
+        self.__LongName = long_name
+        self.__Priority = priority
+        self.__IP = ip
+
+    name = property(lambda self: self.__Name)
+    """
+    """
+
+    mac = property(lambda self: self.__Mac)
+    """
+    """
+
+    long_name = property(lambda self: self.__LongName)
+    """
+    """
+
+    priority = property(lambda self: self.__Priority)
+    """
+        Priority of connected devices, can be equal  0 to 3.
+        0 - Higher priority (Device of administrator).
+        
+    """
+
+    ip = property(lambda self: self.__IP)
+    """
+    """
